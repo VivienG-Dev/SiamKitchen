bfa7563c6f3cc6b8e7d4c768b4beedf0c2094536
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2025-01-30 02:28:14 +0100

    refactor: Create PromotionCard and Promotions components for showcasing meal deals

---
+++ frontend/components/index/PromotionCard.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface Props {
+    title: string;
+    description: string;
+    bgColor: string;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+    <div
+        :class="[bgColor, 'flex-1 flex flex-col gap-2 p-4 rounded-xl h-64 hover:flex-[3] transition-all duration-500 ease-in-out cursor-pointer']">
+        <h2 class="text-2xl font-bold">{{ title }}</h2>
+        <p class="text-lg">{{ description }}</p>
+    </div>
+</template>
---
+++ frontend/components/index/Promotions.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+interface PromotionCard {
+    title: string;
+    description: string;
+    bgColor: string;
+}
+
+const promotions: PromotionCard[] = [
+    {
+        title: 'Family Pack',
+        description: 'Get a family pack of 4 meals for only $100.',
+        bgColor: 'bg-amber-300'
+    },
+    {
+        title: 'Lunch Pack',
+        description: 'Get a lunch pack of 3 meals for only $100.',
+        bgColor: 'bg-green-300'
+    },
+    {
+        title: 'Dinner Pack',
+        description: 'Get a dinner pack of 3 meals for only $100.',
+        bgColor: 'bg-blue-300'
+    }
+];
+
+const sectionRef = ref<HTMLElement | null>(null);
+defineExpose({ scrollIntoView: () => sectionRef.value?.scrollIntoView({ behavior: 'smooth' }) });
+</script>
+
+<template>
+    <section ref="sectionRef" class="text-black px-4 md:px-0 h-[35rem]">
+        <Container class="flex flex-col justify-center h-full">
+            <h1 class="text-3xl md:text-4xl font-bold">Promotions</h1>
+            <p class="text-lg md:text-2xl">We offer a variety of promotions to our customers.</p>
+            <div class="flex flex-row gap-4 mt-8">
+                <IndexPromotionCard v-for="promo in promotions" :key="promo.title" v-bind="promo" />
+            </div>
+        </Container>
+    </section>
+</template>
