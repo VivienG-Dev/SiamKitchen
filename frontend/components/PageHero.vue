1954003c9f0167ced9a7bb2daa3420745d321ae3
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2025-02-02 01:05:41 +0100

    style: Adjust PageHero section height for responsive design

--- frontend/components/PageHero.vue
+++ frontend/components/PageHero.vue
@@ -41,7 +41,8 @@ const dietaryIcons = [
 </script>
 
 <template>
-    <section class="bg-gradient-to-br from-blue-400 via-blue-300 to-green-300 text-white p-4 m-4 h-[20rem] rounded-4xl">
+    <section
+        class="bg-gradient-to-br from-blue-400 via-blue-300 to-green-300 text-white p-4 m-4 h-[15rem] md:h-[20rem] rounded-4xl">
         <Container class="flex flex-col items-center justify-center h-full">
             <div class="flex flex-col items-center">
                 <h1 class="text-3xl md:text-4xl font-bold">{{ title }}</h1>
