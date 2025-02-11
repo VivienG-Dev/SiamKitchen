976d08c559e32015b645c9ac71556f528d0ae229
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2025-02-11 15:41:31 +0100

    refactor: Update MenuPage schema to support nullable image and removing the array

--- frontend/types/menuPage.ts
+++ frontend/types/menuPage.ts
@@ -1,14 +1,12 @@
 import { z } from 'zod'
 import { imageSchema } from './global'
 
-const menuPageImageSchema = z.array(imageSchema)
-
 export const menuPageSchema = z.object({
     id: z.number(),
     documentId: z.string(),
     menuPageTitle: z.string(),
     menuPageDescription: z.string(),
-    menuPageImage: menuPageImageSchema,
+    menuPageImage: imageSchema.nullable(),
     createdAt: z.string(),
     updatedAt: z.string(),
     publishedAt: z.string(),
