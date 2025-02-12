7aa8d97c4b12f30f548bfd67ea883e950571f0d3
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2025-02-12 03:21:15 +0100

    feat: Add Google Fonts with custom typography for headings and body text

--- frontend/app.vue
+++ frontend/app.vue
@@ -2,6 +2,24 @@
 import { useSiteStore } from '~/stores/site'
 const siteStore = useSiteStore()
 await siteStore.fetchSiteData()
+
+useHead({
+  link: [
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com'
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.gstatic.com',
+      crossorigin: 'anonymous'
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Nunito:ital@0;1&family=Playfair+Display:wght@500&display=swap'
+    }
+  ]
+})
 </script>
 
 <template>
