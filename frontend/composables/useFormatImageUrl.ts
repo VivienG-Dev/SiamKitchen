bb623677c306724e4a260b4b148af3cc6d03637b
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2025-02-19 18:59:28 +0100

    fix: getting back to old code, it was working after the restart of the server

--- frontend/composables/useFormatImageUrl.ts
+++ frontend/composables/useFormatImageUrl.ts
@@ -4,36 +4,33 @@ export function useFormatImageUrl(image: { url: string; formats: any }, baseUrl?
         baseUrl = config.public.strapiUrl
     }
 
-    // Force HTTPS by replacing http:// with https://
-    const secureBaseUrl = baseUrl.replace('http://', 'https://')
-
     return {
-        url: `${secureBaseUrl}${image.url}`,
+        url: `${baseUrl}${image.url}`,
         formats: {
             large: image.formats.large
                 ? {
-                    url: `${secureBaseUrl}${image.formats.large.url}`,
+                    url: `${baseUrl}${image.formats.large.url}`,
                     width: image.formats.large.width,
                     height: image.formats.large.height
                 }
                 : undefined,
             medium: image.formats.medium
                 ? {
-                    url: `${secureBaseUrl}${image.formats.medium.url}`,
+                    url: `${baseUrl}${image.formats.medium.url}`,
                     width: image.formats.medium.width,
                     height: image.formats.medium.height
                 }
                 : undefined,
             small: image.formats.small
                 ? {
-                    url: `${secureBaseUrl}${image.formats.small.url}`,
+                    url: `${baseUrl}${image.formats.small.url}`,
                     width: image.formats.small.width,
                     height: image.formats.small.height
                 }
                 : undefined,
             thumbnail: image.formats.thumbnail
                 ? {
-                    url: `${secureBaseUrl}${image.formats.thumbnail.url}`,
+                    url: `${baseUrl}${image.formats.thumbnail.url}`,
                     width: image.formats.thumbnail.width,
                     height: image.formats.thumbnail.height
                 }
